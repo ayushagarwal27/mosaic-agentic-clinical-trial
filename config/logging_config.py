4f6a650a9696ae7524ee45aea05bc6e727b9f0c8
ayush <ayushagarwal.dev@gmail.com>
2026-08-22 15:53:49 +0530

    add: env example and logging config

---
+++ config/logging_config.py
@@ -0,0 +1,25 @@
+import logging
+import sys
+
+def setup_logging(name:str) -> logging.Logger:
+    """
+    Called at top of each py file
+    """
+    logger = logging.getLogger(name)
+    
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+
+    formatter = logging.Formatter(
+        fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False
+
+
